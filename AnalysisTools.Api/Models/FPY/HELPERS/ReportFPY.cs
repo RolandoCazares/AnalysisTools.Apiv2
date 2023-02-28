d06e6f36942d8007529aec32de9c63c8e3be0c9f
Huawei Rolando <rolando_99-2011@hotmail.com>
2023-02-28 07:33:48 -0700

    se agrego modelo para reporte de fpy

---
+++ AnalysisTools.Api/Models/FPY/HELPERS/ReportFPY.cs
@@ -0,0 +1,13 @@
+﻿namespace analysistools.api.Models.FPY.HELPERS
+{
+    public class ReportFPY
+    {
+        public string Var { get; set; }
+
+        public int TotalProduced { get; set; }
+
+        public int TotalFailures { get; set; }
+
+        public double FPY { get; set; }
+    }
+}
